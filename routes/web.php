15038569258e9589fd8e4c04f43b2ce5277a87d7
hoang <buiduchoang2101@gmail.com>
2024-11-29 09:54:15 +0700

    update add to cart in wishlist

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,7 @@ Route::get('/cart', [CartController::class, 'index'])->name('cart');
 Route::patch('/cart/update/{id}', [CartController::class, 'update'])->name('cart.update');
 Route::post('/cart/increase/{id}', [CartController::class, 'increase'])->name('cart.increase');
 Route::post('/cart/decrease/{id}', [CartController::class, 'decrease'])->name('cart.decrease');
+Route::post('/wishlist-to-cart/{id}', [CartController::class, 'wishlistToCart'])->name('wishlist.to.cart');
 
 
 // Route cho wishlist
